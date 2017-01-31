0bf8ada4436b011fe41a41323ac56f3242573650
m4b <m4b.github.io@gmail.com>
2017-01-30 22:28:05 -0800

    update benches

--- benches/bench.rs
+++ benches/bench.rs
@@ -14,20 +14,20 @@ fn bench_pread_ctx_vec(b: &mut test::Bencher) {
     b.iter(|| {
         let data = black_box(&vec[..]);
         for val in data.chunks(2) {
-            let _: Result<u16, _> = black_box(val.pread_into(0));
+            let _: Result<u16, _> = black_box(val.pread(0));
         }
     });
     b.bytes = vec.len() as u64;
 }
 
 #[bench]
-fn bench_pread_unwrap(b: &mut test::Bencher) {
+fn bench_pread_with_unwrap(b: &mut test::Bencher) {
     use scroll::{Pread, LE};
     const NITER: i32 = 100_000;
     b.iter(|| {
         for _ in 1..NITER {
             let data: &[u8] = &black_box([1, 2]);
-            let _: u16 = black_box(data.pread(0, LE).unwrap());
+            let _: u16 = black_box(data.pread_with(0, LE).unwrap());
         }
     });
     b.bytes = 2 * NITER as u64;
@@ -40,20 +40,20 @@ fn bench_pread_vec(b: &mut test::Bencher) {
     b.iter(|| {
         let data = black_box(&vec[..]);
         for val in data.chunks(2) {
-            let _: Result<u16, _> = black_box(val.pread(0, LE));
+            let _: Result<u16, _> = black_box(val.pread_with(0, LE));
         }
     });
     b.bytes = vec.len() as u64;
 }
 
 #[bench]
-fn bench_pread_into_unwrap(b: &mut test::Bencher) {
+fn bench_pread_unwrap(b: &mut test::Bencher) {
     use scroll::{Pread};
     const NITER: i32 = 100_000;
     b.iter(|| {
         for _ in 1..NITER {
             let data = black_box([1, 2]);
-            let _: u16 = black_box(data.pread_into(0)).unwrap();
+            let _: u16 = black_box(data.pread(0)).unwrap();
         }
     });
     b.bytes = 2 * NITER as u64;
@@ -80,7 +80,7 @@ fn bench_gread_vec(b: &mut test::Bencher) {
         let data = black_box(&vec[..]);
         for val in data.chunks(2) {
             let mut offset = 0;
-            let _: Result<u16, _> = black_box(val.gread_into(&mut offset));
+            let _: Result<u16, _> = black_box(val.gread(&mut offset));
         }
     });
     b.bytes = vec.len() as u64;
@@ -101,7 +101,7 @@ fn bench_gread_unsafe(b: &mut test::Bencher) {
 }
 
 #[bench]
-fn bench_parallel_pread(b: &mut test::Bencher) {
+fn bench_parallel_pread_with(b: &mut test::Bencher) {
     use scroll::{Pread, LE};
     use rayon::prelude::*;
     let vec = vec![0u8; 1_000_000];
@@ -109,7 +109,7 @@ fn bench_parallel_pread(b: &mut test::Bencher) {
     b.iter(|| {
         let data = black_box(&vec[..]);
         nums.par_iter().for_each(| offset | {
-            let _: Result<u16, _> = black_box(data.pread(*offset, LE));
+            let _: Result<u16, _> = black_box(data.pread_with(*offset, LE));
         });
     });
     b.bytes = vec.len() as u64;
