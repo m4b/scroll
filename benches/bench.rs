a9abfa24e2b58613ad72dd6270869debb4f59435
m4b <m4b.github.io@gmail.com>
2018-09-22 13:25:59 -0700

    bench: remove commented code

--- benches/bench.rs
+++ benches/bench.rs
@@ -159,29 +159,3 @@ fn bench_byteorder(b: &mut test::Bencher) {
     });
     b.bytes = 2 * NITER as u64;
 }
-
-// #[bench]
-// fn bench_byteio_vec(b: &mut test::Bencher) {
-//     use byteio::ReadBytesExt;
-//     let vec = vec![0u8; 1_000_000];
-//     b.iter(|| {
-//         let data = black_box(&vec[..]);
-//         for mut val in data.chunks(2) {
-//             let _: Result<u16, _> = black_box(val.read_as::<byteio::LittleEndian>());
-//         }
-//     });
-//     b.bytes = vec.len() as u64;
-// }
-
-// #[bench]
-// fn bench_byteio(b: &mut test::Bencher) {
-//     use byteio::ByteOrder;
-//     const NITER: i32 = 100_000;
-//     b.iter(|| {
-//         for _ in 1..NITER {
-//             let data = black_box([1, 2]);
-//             let _: u16 = black_box(byteio::LittleEndian::from_bytes(data));
-//         }
-//     });
-//     b.bytes = 2 * NITER as u64;
-// }
