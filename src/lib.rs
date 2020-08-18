e94c0d112456aa75c5b51f5a799fb3bc68d76af5
Gregor Reitzenstein <me@dequbed.space>
2020-08-18 14:38:35 +0200

    Start cleaning up the documentation

--- src/lib.rs
+++ src/lib.rs
@@ -12,87 +12,137 @@
 //!
 //! ```
 //!
-//! Scroll is a library for efficiently and easily reading/writing types from byte arrays. All the builtin types are supported, e.g., `u32`, `i8`, etc., where the type is specified as a type parameter, or type inferred when possible. In addition, it supports zero-copy reading of string slices, or any other kind of slice.  The library can be used in a no_std context as well; the [Error](enum.Error.html) type only has the `IO` and `String` variants if the default features are used, and is `no_std` safe when compiled without default features.
+//! Scroll is a library for easily and efficiently reading/writing types from data containers like
+//! byte arrays.
 //!
-//! There are 2 traits for reading that you can import:
+//! ## Easily:
 //!
-//! 1. [Pread](trait.Pread.html), for reading (immutable) data at an offset (potentially incrementing said offset using [gread](trait.Pread.html#method.gread));
-//! 2. [IOread](trait.IOread.html), for reading _simple_ data out of a `std::io::Read` based interface, e.g., a stream. (**Note**: only available when compiled with `std`)
+//! Scroll sets down a number of traits:
 //!
-//! Each of these interfaces also have their corresponding writer versions as well, i.e. [Pwrite](trait.Pwrite.html) and [IOwrite](trait.IOwrite.html), respectively.
+//! [FromCtx](ctx/trait.FromCtx.html), [IntoCtx](ctx/trait.IntoCtx.html) and
+//! [TryFromCtx](ctx/trait.TryFromCtx.html); to be implemented on custom types to allow reading,
+//! writing, and potentially fallible reading respectively.
 //!
-//! Most familiar will likely be the `pread` method from the `Pread` trait (inspired from the C function), which in our case takes an immutable reference to self, an immutable offset to read at, (and _optionally_ a parsing context, more on that later), and then returns the deserialized value.
+//! [Pread](trait.Pread.html) and [Pwrite](trait.Pwrite.html) which are implemented on data
+//! containers such as byte arrays to define how to read or respectively write types implementing
+//! the *Ctx traits above.
+//! In addition scroll also defines [IOread](trait.IOread.html) and
+//! [IOwrite](trait.IOwrite.html) with additional constraits that then allow reading and writing
+//! from `std::io` [Read](https://doc.rust-lang.org/nightly/std/io/trait.Read.html) and
+//! [Write](https://doc.rust-lang.org/nightly/std/io/trait.Write.html).
 //!
-//! Because self is immutable, _**all** reads can be performed in parallel_ and hence are trivially parallelizable.
 //!
-//! For most usecases, you can use [scroll_derive](https://docs.rs/scroll_derive) to annotate your types with `derive(Pread, Pwrite, IOread, IOwrite, SizeWith)` to automatically add sensible derive defaults, and you should be ready to roll.  For more complex usescases, you can implement the conversion traits yourself, see the [context module](ctx/index.html) for more information.
+//! In most cases you can use [scroll_derive](https://docs.rs/scroll_derive) to derive sensible
+//! defaults for `Pread`, `Pwrite`, their IO counterpart and `SizeWith`.  More complex situations
+//! call for manual implementation of those traits; refer to [the ctx module](ctx/index.html) for
+//! details.
 //!
-//! # Example
 //!
-//! A simple example demonstrates its flexibility:
+//! ## Efficiently:
+//!
+//! Reading Slices — including [&str](https://doc.rust-lang.org/std/primitive.str.html) — supports
+//! zero-copy. Scroll is designed with a `no_std` context in mind; every dependency on `std` is
+//! cfg-gated and errors need not allocate.
+//!
+//! Reads by default take only immutable references wherever possible, allowing for trivial
+//! parallelization.
+//!
+//! # Examples
+//!
+//! Let's start with a simple example
 //!
 //! ```rust
-//! use scroll::{ctx, Pread, LE};
+//! use scroll::{ctx, Pread};
+//!
+//! // Let's first define some data, cfg-gated so our assertions later on hold.
+//! #[cfg(target_endian = "little")]
 //! let bytes: [u8; 4] = [0xde, 0xad, 0xbe, 0xef];
+//! #[cfg(target_endian = "big")]
+//! let bytes: [u8; 4] = [0xef, 0xbe, 0xad, 0xde];
 //!
-//! // reads a u32 out of `b` with the endianness of the host machine, at offset 0, turbofish-style
-//! let number: u32 = bytes.pread::<u32>(0).unwrap();
-//! // ...or a byte, with type ascription on the binding.
-//! let byte: u8 = bytes.pread(0).unwrap();
+//! // We can read a u32 from the array `bytes` at offset 0. 
+//! // This will use a default context for the type being parsed;
+//! // in the case of u32 this defines to use the host's endianess.
+//! let number = bytes.pread::<u32>(0).unwrap();
+//! assert_eq!(number, 0xefbeadde);
 //!
-//! //If the type is known another way by the compiler, say reading into a struct field, we can omit the turbofish, and type ascription altogether!
 //!
-//! // If we want, we can explicitly add a endianness to read with by calling `pread_with`.
-//! // The following reads a u32 out of `b` with Big Endian byte order, at offset 0
+//! // Similarly we can also read a single byte at offset 2
+//! // This time using type ascription instead of the turbofish (::<>) operator.
+//! let byte: u8 = bytes.pread(2).unwrap();
+//! assert_eq!(byte, 0xbe);
+//!
+//!
+//! // If required we can also provide a specific parsing context; e.g. if we want to explicitly
+//! // define the endianess to use:
 //! let be_number: u32 = bytes.pread_with(0, scroll::BE).unwrap();
-//! // or a u16 - specify the type either on the variable or with the beloved turbofish
-//! let be_number2 = bytes.pread_with::<u16>(2, scroll::BE).unwrap();
+//! assert_eq!(be_number, 0xdeadbeef);
 //!
-//! // Scroll has core friendly errors (no allocation). This will have the type `scroll::Error::BadOffset` because it tried to read beyond the bound
-//! let byte: scroll::Result<i64> = bytes.pread(0);
+//! let be_number16 = bytes.pread_with::<u16>(1, scroll::BE).unwrap();
+//! assert_eq!(be_number16, 0xadbe);
 //!
-//! // Scroll is extensible: as long as the type implements `TryWithCtx`, then you can read your type out of the byte array!
 //!
-//! // We can parse out custom datatypes, or types with lifetimes
-//! // if they implement the conversion trait `TryFromCtx`; here we parse a C-style \0 delimited &str (safely)
-//! let hello: &[u8] = b"hello_world\0more words";
+//! // Reads may fail; in this example due to a too large read for the given container.
+//! // Scroll's error type does not by default allocate to work in environments like no_std.
+//! let byte_err: scroll::Result<i64> = bytes.pread(0);
+//! assert!(byte_err.is_err());
+//!
+//!
+//! // We can parse out custom datatypes, or types with lifetimes, as long as they implement
+//! // conversion trait `TryFromCtx/FromCtx`.
+//! // Here we use the default context for &str which parses are C-style '\0'-delimited string.
+//! let hello: &[u8] = b"hello world\0more words";
 //! let hello_world: &str = hello.pread(0).unwrap();
-//! assert_eq!("hello_world", hello_world);
+//! assert_eq!("hello world", hello_world);
 //!
-//! // ... and this parses the string if its space separated!
-//! use scroll::ctx::*;
-//! let spaces: &[u8] = b"hello world some junk";
-//! let world: &str = spaces.pread_with(6, StrCtx::Delimiter(SPACE)).unwrap();
-//! assert_eq!("world", world);
+//! // We can again provide a custom context; for example to parse Space-delimited strings.
+//! let hello2: &[u8] = b"hello world\0more words";
+//! let world: &str = hello2.pread_with(6, ctx::StrCtx::Delimiter(ctx::SPACE)).unwrap();
+//! assert_eq!("world\0more", world);
 //! ```
 //!
-//! # `std::io` API
+//! ## `std::io` API
 //!
-//! Scroll can also read/write simple types from a `std::io::Read` or `std::io::Write` implementor. The  built-in numeric types are taken care of for you.  If you want to read a custom type, you need to implement the [FromCtx](ctx/trait.FromCtx.html) (_how_ to parse) and [SizeWith](ctx/trait.SizeWith.html) (_how_ big the parsed thing will be) traits.  You must compile with default features. For example:
+//! Scroll also allows reading from `std::io`. For this the types to read need to implement
+//! [FromCtx](ctx/trait.FromCtx.html) and [SizeWith](ctx/trait.SizeWith.html).
 //!
 //! ```rust
 //! use std::io::Cursor;
-//! use scroll::IOread;
-//! let bytes_ = [0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00, 0xef,0xbe,0x00,0x00,];
-//! let mut bytes = Cursor::new(bytes_);
+//! use scroll::{IOread, ctx, Endian};
+//! let bytes = [0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00, 0xef,0xbe,0x00,0x00,];
+//! let mut cursor = Cursor::new(bytes);
 //!
-//! // this will bump the cursor's Seek
-//! let foo = bytes.ioread::<u64>().unwrap();
-//! // ..ditto
-//! let bar = bytes.ioread::<u32>().unwrap();
+//! // IOread uses std::io::Read methods, thus the Cursor will be incremented on these reads:
+//! let prev = cursor.position();
+//!
+//! let integer = cursor.ioread_with::<u64>(Endian::Little).unwrap();
+//!
+//! let after = cursor.position();
+//!
+//! assert!(prev < after);
+//!
+//! // SizeWith allows us to define a context-sensitive size of a read type:
+//! // Contexts can have different instantiations; e.g. the `Endian` context can be either Little or
+//! // Big. This is useful if for example the context contains the word-size of fields to be
+//! // read/written, e.g. switching between ELF32 or ELF64 at runtime.
+//! let size = <u64 as ctx::SizeWith<Endian>>::size_with(&Endian::Little) as u64;
+//! assert_eq!(prev + size, after);
 //! ```
 //!
-//! Similarly, we can write to anything that implements `std::io::Write` quite naturally:
+//! In the same vein as IOread we can use IOwrite to write a type to anything implementing
+//! `std::io::Write`:
 //!
 //! ```rust
-//! use scroll::{IOwrite, LE, BE};
-//! use std::io::{Write, Cursor};
+//! use std::io::Cursor;
+//! use scroll::{IOwrite};
 //!
-//! let mut bytes = [0x0u8; 10];
+//! let mut bytes = [0x0u8; 5];
 //! let mut cursor = Cursor::new(&mut bytes[..]);
-//! cursor.write_all(b"hello").unwrap();
-//! cursor.iowrite_with(0xdeadbeef as u32, BE).unwrap();
-//! assert_eq!(cursor.into_inner(), [0x68, 0x65, 0x6c, 0x6c, 0x6f, 0xde, 0xad, 0xbe, 0xef, 0x0]);
+//!
+//! // This of course once again increments the cursor position
+//! cursor.iowrite_with(0xdeadbeef as u32, scroll::BE).unwrap();
+//!
+//! assert_eq!(cursor.into_inner(), [0xde, 0xad, 0xbe, 0xef, 0x0]);
 //! ```
 //!
 //! # Advanced Uses
